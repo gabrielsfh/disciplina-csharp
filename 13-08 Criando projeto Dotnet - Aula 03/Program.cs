ba6094cc4af1cee84dfa540cabf9f2eca86ce2c2
gabrielsanic <90591801+gabrielsanic@users.noreply.github.com>
2024-08-13 23:10:03 +0000

    13-08

---
+++ 13-08 Criando projeto Dotnet - Aula 03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args){
+        
+        Console.WriteLine("Cadastro de Pessoa!");
+        Console.Write("Nome: ");
+        string nomePessoa = Console.ReadLine();
+        Console.Write("Idade: ");
+        int idadePessoa = int.Parse(Console.ReadLine()); //Converte o valor String para Int
+
+        Pessoa pessoa = new Pessoa(nomePessoa, idadePessoa);
+
+        Console.WriteLine(pessoa.ToString());
+
+    }
+}
