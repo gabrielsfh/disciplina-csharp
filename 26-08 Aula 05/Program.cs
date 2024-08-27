49a4621c4469212574d5bf1e83a8a499a27da8c4
gabrielsanic <90591801+gabrielsanic@users.noreply.github.com>
2024-08-27 00:33:20 +0000

    aula 05

---
+++ 26-08 Aula 05/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args){
+        // Conversão implicita de INT para DOUBLE
+        int numInt = 45;
+        double numDouble = numInt;
+        Console.WriteLine("Conversão implícita de int para double:");
+        Console.WriteLine($"Int: {numInt}, Double: {numDouble}");
+
+        // Conversão explicita usando o Convert
+        double valorDouble = 7.45;
+        int valorConvertidoInt = valorConvertidoInt.ToInt32(valorDouble);
+        string textoValor = "123";
+        int textoConvertidoInt = textoConvertidoInt.ToInt32(textoValor);
+
+        Console.WriteLine($"Double: {valorDouble}, Inteiro: {valorConvertidoInt}"
+        Console.WriteLine($"String: {textoValor}, Inteiro {textoConvertidoInt}");
+    }
+}
+
+
+
