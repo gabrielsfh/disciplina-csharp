b2b36c53ad4ee724d8254b6b1b8762de3c00be34
gabrielsanic <90591801+gabrielsanic@users.noreply.github.com>
2024-12-31 03:12:35 +0000

    remoção arquivos redundantes

--- prova2/Disciplina.cs
+++
@@ -1,9 +0,0 @@
-namespace prova2
-{
-public class Disciplina{
-    public int Id {get; set;}
-    public string NomeDisciplina {get; set;}
-    public int CodigoDisciplina {get; set;}
-    public IList<Aluno> Alunos {get; set;} = new List<Aluno>();
-}
-}
